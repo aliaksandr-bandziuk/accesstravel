04c12d1add178395196110098d5ab7cd039d4683
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2023-01-12 22:51:27 +0100

    feat: create first page

---
+++ src/js/files/swiper-tags.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper-tags', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 7,
+  spaceBetween: 10,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.nex-btn',
+  },
+
+});
