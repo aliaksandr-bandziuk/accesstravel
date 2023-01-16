4641f92fd8fc6b9739cce4b1077979f9dbfcacc0
Aliaksandr Bandziuk <62721784+aliaksandr-bandziuk@users.noreply.github.com>
2023-01-16 12:31:03 +0100

    Updates

--- js/app.js
+++ js/app.js
@@ -2449,6 +2449,24 @@
         galleryThumbs.on("transitionStart", (function() {
             galleryMain.slideTo(galleryThumbs.activeIndex);
         }));
+        const shareBtn = document.querySelector(".share-btn");
+        const shareBlock = document.querySelector(".buttons");
+        const popupShare = () => {
+            const toggle = () => {
+                shareBlock.classList.toggle("hidden");
+            };
+            const remove = () => {
+                shareBlock.classList.add("hidden");
+            };
+            document.addEventListener("click", (e => {
+                const target = e.target;
+                target === shareBtn ? toggle() : target !== shareBlock ? remove() : false;
+            }));
+            document.addEventListener("keydown", (e => {
+                if ("Escape" === e.key) remove();
+            }));
+        };
+        popupShare();
         window["FLS"] = true;
         isWebp();
         menuInit();
